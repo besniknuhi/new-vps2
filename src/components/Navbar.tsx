1f31756f28dae6c0e7f36bab6c4a313a5b3561c1
unknown <nuhibesnik@gmail.com>
2024-07-16 16:23:20 +0200

    typing err

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -113,13 +113,13 @@ export default function Navbar() {
         </div>
         <div className="flex flex-row w-full items-start gap-2.5 px-7 pt-10 ">
           <Link
-            href="/login"
+            href="/signup"
             className="bg-neutral-900 relative z-10 hover:bg-black/90 border border-transparent text-white text-sm md:text-sm transition font-medium duration-200 rounded-full px-4 py-2 flex items-center justify-center"
           >
             Sign up
           </Link>
           <Link
-            href="/signup"
+            href="/login"
             className="relative z-10 bg-transparent hover:bg-gray-100 border border-transparent text-black text-sm md:text-sm transition font-medium duration-200 rounded-full px-4 py-2 flex items-center justify-center"
           >
             Login
