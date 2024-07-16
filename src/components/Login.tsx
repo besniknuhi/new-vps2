6cb27c55b3bd078b2b68649c61026f7d67def8bc
unknown <nuhibesnik@gmail.com>
2024-07-16 16:32:29 +0200

    typing error

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -81,7 +81,7 @@ export default function Login() {
                 </div>
                 <div>
                   <button className="bg-neutral-900 relative z-10 hover:bg-black/90 border border-transparent text-white text-sm md:text-sm transition font-medium duration-200 rounded-full px-4 py-2 flex items-center justify-center shadow-[0px_-1px_0px_0px_#FFFFFF40_inset,_0px_1px_0px_0px_#FFFFFF40_inset] w-full">
-                    Sign Up
+                    Login
                   </button>
                   <p className="text-sm text-center mt-4">
                     Don&apos;t have an account?{" "}
